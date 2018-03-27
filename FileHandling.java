34f08acd012beceeedd22366b0f1a273f81f985d
Raghav Awasthi <raghavawasthi2014@gmail.com>
2018-03-27 08:57:41 +0530

    Add files via upload

---
+++ FileHandling.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class FileHandling
+{
+public static void main(String ar[])
+{
+Scanner sc= new Scanner(System.in);
+System.out.println("this is the content to be written in the output File");
+int n=sc.nextInt();
+System.out.println("this is the content to be written in the output File"+n);
+
+}
+}
